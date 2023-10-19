2fd5c69388e87d33e568c88b1d93f26b4fd4c91b
unknown <sathish995325@gmail.com>
2023-10-19 14:43:19 +0530

    added file foo.java

---
+++ foo.java
@@ -0,0 +1,12 @@
+changes by Sathish
+
+class foo{
+	System.out.println("this is code by Sathish MBBS");
+}
+
+changes  by krishnakumar
+
+class {
+	System.out.println("this is code by krishna");
+}
+
